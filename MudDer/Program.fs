1b179b54e95ef03374a6043673fa78de00a31fad
ImaginaryDevelopment <imaginarydevelopment@googlemail>
2014-10-31 14:58:47 -0400

    cleaning

--- MudDer/Program.fs
+++ MudDer/Program.fs
@@ -9,14 +9,20 @@ type Die=
     | D20 = 20
     //with 
     //member x.Roll (rng:int->int) = rng(x)
+[<Measure>]
+type Health
 
 // See the 'F# Tutorial' project for more help.
-type Monster = { Health:int; Name:string; (* THAC0: byte;*) Dmg: Die*byte seq}
+type Monster = { Health:int<Health>; Name:string; (* THAC0: byte;*) Dmg: Die*byte seq}
 
-type State = { Room:string; Xp:int; Level:int; X:int; Y:int; MoveCount:int; Monster:Monster option; Damage: Die} with
-    static member Initial = {Room="start"; Xp=0; Level =1; X=0; Y=0; MoveCount =0; Monster = None; Damage = Die.D4}
+type State = { Room:string; Xp:int; Level:int; X:int; Y:int; MoveCount:int; Monster:Monster option; Health: int<Health>; Damage: Die} with
+    static member Initial = {Room="start"; Xp=0; Level =1; X=0; Y=0;Health = 4<Health>; MoveCount =0; Monster = None; Damage = Die.D4}
+
 type Event =
     | ArrivalMessage of string
+    | MonsterArrival of Monster
+    | TreasureFound of string
+
 type Command = 
     |West
     |East
@@ -33,14 +39,13 @@ type Command =
 // post = 
 //    mailbox.PostAndReply( fun replyChannel -> Query replyChannel)
 //    mailbox.PostAndReply( fun replyChannel -> Update(Open(doOpen),replyChannel))
-type Message = 
-    |Cmd of Command*State*AsyncReplyChannel<Reply*State>
+type Message = Command*State*AsyncReplyChannel<Reply*State>
     //|East of State*AsyncReplyChannel<Reply*State>
 and
     Reply =
     | RoomChange
-    | Message of string
-
+    | Msg of string
+    | Death of string
 
 [<EntryPoint>]
 let main argv = 
@@ -65,19 +70,21 @@ let main argv =
                 async{
                     
                     let! message = (* printfn"waiting for nextMessage"; *) inbox.Receive()
-                    let msg (rc:AsyncReplyChannel<Reply*State>) (state:State) s = rc.Reply(Message s, {state with MoveCount = state.MoveCount + 1})
+
+                    let msg (rc:AsyncReplyChannel<Reply*State>) (state:State) s = rc.Reply(Msg s, {state with MoveCount = state.MoveCount + 1})
                     match message with
-                    | Cmd(West,state,replyChannel) -> replyChannel.Reply( RoomChange,{state with Room = "west"})
-                    | Cmd(East,state,replyChannel) -> replyChannel.Reply( if state.Room="west" then printfn "going west"; RoomChange,{state with Room = "start"} else Message "Can't go that way dave",state)
-                    | Cmd(Wait,state,replyChannel) -> msg replyChannel state "What are you waiting for? This dungeon isn't going to explore itself"
-                    | Cmd(Attack,state,replyChannel) -> 
+                    | (West,state,replyChannel) -> replyChannel.Reply( RoomChange,{state with Room = "west"})
+                    | (East,state,replyChannel) -> replyChannel.Reply( if state.Room="west" then printfn "going west"; RoomChange,{state with Room = "start"} else Msg "Can't go that way dave",state)
+                    | (Wait,state,replyChannel) -> msg replyChannel state "What are you waiting for? This dungeon isn't going to explore itself"
+                    | (Attack,state,replyChannel) -> 
                         if state.Monster.IsSome then 
-                            let monsterHealth = state.Monster.Value.Health - rng state.Damage
-                            if monsterHealth <= 0 then
+                            let monsterHealth = state.Monster.Value.Health - 1<Health> * rng state.Damage
+                            if monsterHealth <= 0<_> then
                                 let result = {state with Xp = state.Xp + 1; MoveCount = state.MoveCount+1; Monster = None }
-                                msg replyChannel result " You hit %s and have slain it"
+                                msg replyChannel result <| sprintf " You hit %s and have slain it" state.Monster.Value.Name
                         else
-                            msg replyChannel state "hello!"                                      //counter attack
+                            let result = { state with Health = state.Health }
+                            msg replyChannel state "The Monster attacks you!"  //counter attack
 
                     do! loop()
                 }
@@ -86,31 +93,26 @@ let main argv =
         processor.Start()
         processor
     let rec takeInput (rng:Die->int) (state:State) (s:string) : bool*State = 
-        // printfn "Found input! %s" s
-        // post = 
-        //    mailbox.PostAndReply( fun replyChannel -> Query replyChannel)
-        //    mailbox.PostAndReply( fun replyChannel -> Update(Open(doOpen),replyChannel))
 
-        let op command  = Some <| fun replyChannel -> (Cmd( command ,state,replyChannel))
+        let op command  = Some <| fun replyChannel -> ( command ,state,replyChannel)
         let inputMap = match s.ToLowerInvariant() with 
-                                                                | "west" -> Some <| fun replyChannel -> Cmd( Command.West, state,replyChannel)
+                                                                | "west" -> Some <| fun replyChannel -> Command.West, state,replyChannel
                                                                 | "east" -> op East
                                                                 | "quit" | "exit" -> (* printfn"found quit"; *) None
                                                                 | "attack" -> op Attack
-                                                                        
                                                                 |_ -> (* printfn "no op";*) op Wait
         match inputMap with 
         |Some msg -> 
                     let reply,newState  = mailbox.PostAndReply inputMap.Value
                     match reply,newState with
                                | RoomChange,_ -> true,newState
-                               | Message s,_ -> printfn "%s" s; true, newState
+                               | Msg s,_ -> printfn "%s" s; true, newState
                                | _ -> true, newState
         |None -> false,state
 
     let rec msgPump (state:State):State option = 
         //printfn "unfolding!"
-        let shouldContinue,newState = printfn "Command?"; takeInput rng state (Console.ReadLine())
+        let shouldContinue,newState = printfn "Command?"; takeInput rng state <| Console.ReadLine()
         //Console.Out.Flush()
         //System.Threading.Thread.Sleep(100)
         if shouldContinue then (* printfn "continuing adventure!"; *) msgPump newState
